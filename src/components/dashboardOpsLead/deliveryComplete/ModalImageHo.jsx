96dbbc3318ef88fe1a697fe96ee6b0fea344c8f5
chandra darmawan <chandradarmawan17@gmail.com>
2023-07-15 23:48:28 +0700

    redesign HO Document Image

--- src/components/dashboardOpsLead/deliveryComplete/ModalImageHo.jsx
+++ src/components/dashboardOpsLead/deliveryComplete/ModalImageHo.jsx
@@ -9,56 +9,116 @@ import {
     CCol,
     CCardImage,
     CCard,
+    CCarousel,
+    CCarouselItem,
+    CImage,
+    CInputGroup,
+    CFormInput,
+    CButton,
+    CCardHeader,
+    CCardBody,
+    CCollapse,
 } from '@coreui/react'
 import ButtonCancel from 'src/components/custom/button/ButtonCancel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import debounce from "lodash.debounce"
+import CIcon from '@coreui/icons-react';
+import { cilImage } from '@coreui/icons';
 
 function ModalOpenMap({ open, setOpen, data }) {
     const detail = data.getAllEvidences
     const [openImageDetail, setOpenImageDetail] = useState(false);
     const [path, setPath] = useState('')
+    const [visible, setVisible] = useState({})
 
     const handlePreviewPhoto = (path) => {
         setOpenImageDetail(true)
         setPath(path)
     }
 
+    const handleOnchange = debounce(
+        (e) => {
+            //   const { value } = e.target;
+            //   setFilteredProject(Dashboard?.listProject)
+
+            //   if (value?.length > 2) {
+            //     setSearchProject(value.toLowerCase())
+            //   }
+        },
+        // searchProject ? 500 : 0
+    )
+
+    const handleVisible = (index) => {
+        setVisible((prev) => ({
+            ...prev,
+            [index]: visible?.[index] ? !visible?.[index] : true
+        }));
+    }
+
     return (
         <>
             <CModal
-                size="xl"
+                size="lg"
                 visible={open}
-                onClose={() => setOpen}
+                onClose={() => {
+                    setOpen(false)
+                    setVisible({})
+                }}
             >
                 <CModalHeader>
                     <CModalTitle>IMAGES</CModalTitle>
                 </CModalHeader>
                 <CModalBody>
-                    <CRow >
-                        <CCol sm={12}>
-                            <ul>
-                                {detail?.map((row, index) => (
-                                    <li key={index + 1}>
-                                        <p>{row.checklistName}</p>
-                                        <CRow>
-                                            {row.getEvidenceChecklists?.map((row1, index1) => (
-                                                <CCol key={index1} sm={2}>
-                                                    <CCard className='mb-2'>
-                                                        <CCardImage
-                                                            orientation="top"
-                                                            src={row1?.deliveryEvidencePath}
-                                                            width={10}
-                                                            height={50}
-                                                            onClick={(e) => handlePreviewPhoto(row1?.deliveryEvidencePath)}
-                                                        />
-                                                    </CCard>
-                                                </CCol>
-                                            ))}
-                                        </CRow>
-                                    </li>
-                                ))}
-                            </ul>
+                    <CRow>
+                        <CCol>
+                            <CInputGroup className="mb-3">
+                                <CFormInput type="text" name="search" placeholder="Search" onChange={handleOnchange} />
+                                <CButton type="button" className='colorBtnIcon-white'>
+                                    <FontAwesomeIcon icon={faSearch} size='sm' />
+                                </CButton>
+                            </CInputGroup>
                         </CCol>
                     </CRow>
+                    {detail?.map((row, index) => (
+                        <CRow key={index} className='mb-2'>
+                            <CCol>
+                                <CRow>
+                                    <CCol>
+                                        <CCard onClick={() => handleVisible(index)}>
+                                            <CCardHeader>
+                                                <p><CIcon icon={cilImage} className="me-2 text-warning" />{row.checklistName}</p>
+                                            </CCardHeader>
+                                        </CCard>
+                                    </CCol>
+                                </CRow>
+                                <CRow>
+                                    <CCol>
+                                        <CCollapse visible={visible?.[index]}>
+                                            <CCard>
+                                                <CCardBody>
+                                                    <CCarousel controls indicators>
+                                                        {row.getEvidenceChecklists?.map((row1, index1) => (
+                                                            <CCarouselItem key={index1}>
+                                                                <CImage
+                                                                    className="d-block w-100"
+                                                                    src={row1?.deliveryEvidencePath}
+                                                                    alt={index1}
+                                                                    // height={400}
+                                                                    onClick={(e) => handlePreviewPhoto(row1?.deliveryEvidencePath)}
+                                                                />
+                                                            </CCarouselItem>
+                                                        ))}
+                                                    </CCarousel>
+                                                </CCardBody>
+                                            </CCard>
+                                        </CCollapse>
+                                    </CCol>
+                                </CRow>
+
+                            </CCol>
+                        </CRow>
+                    ))}
                 </CModalBody>
                 <CModalFooter>
                     < CRow >
