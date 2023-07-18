7ef2e3bdc98f50ad27f36ab6d9f8c7a1f1e3dcac
candradarmawan <chandradarmawan17@gmail.com>
2023-07-18 14:22:40 +0700

    ajusment helper handleRoleDashboard

--- src/helper/urlHelper.js
+++ src/helper/urlHelper.js
@@ -1,5 +1,18 @@
-export const handleRoleDashboard = (url) => {
-    if (url === '/usr/dashboardOpsLead') return '/dashboard-ops-lead'
-    if (url === '/usr/dashboardKU') return '/dashboard'
-    if (url === '/usr/dashboardWHTeam') return '/dashboard-wh'
+export const handleRoleDashboard = (type) => {
+    const navigate = [
+        {
+            type: '/usr/dashboardOpsLead',
+            url: '/dashboard-ops-lead'
+        },
+        {
+            type: '/usr/dashboardKU',
+            url: '/dashboard'
+        },
+        {
+            type: '/usr/dashboardWHTeam',
+            url: '//dashboard-wh'
+        }
+    ]
+    const { url } = navigate.find(e => e.type === type)
+    return url
 };
