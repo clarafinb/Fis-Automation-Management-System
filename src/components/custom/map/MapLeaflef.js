391e04c0911e38d105e08f951b38f924de32d1aa
chandra darmawan <cdarmawan@juraganmaterial.id>
2023-08-04 18:18:38 +0700

    update map

--- src/components/custom/map/MapLeaflef.js
+++ src/components/custom/map/MapLeaflef.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import L from 'leaflet';
 
-const MapComponent = ({latlong, id}) => {
-    
+const MapComponent = ({ latlong, id }) => {
+
+  const iconPerson = new L.Icon({
+    iconUrl: 'icon/maps-icon-2.svg',
+    iconRetinaUrl: 'icon/maps-icon-2.svg',
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(40, 40),
+    className: 'leaflet-div-icon'
+  });
+
   return (
     <MapContainer
       key={id}
@@ -11,10 +24,12 @@ const MapComponent = ({latlong, id}) => {
       style={{ height: '400px', width: '100%' }}
     >
       <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={latlong} />
+      <Marker
+        position={latlong}
+        icon={iconPerson} />
     </MapContainer>
   )
 };
